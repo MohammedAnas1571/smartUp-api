85bf9c3bccd4724234b87596a11d31c4f6e4b7f0
Mohammed Anas <mohammedanas1571@gmail.com>
2024-08-07 23:37:55 +0530

    origin is created

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use((err, req, res, next) => {
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin:["http://localhost:5173","https://smartup-seven.vercel.app/"],
+    origin:["http://localhost:5173","https://smartup-seven.vercel.app"],
     credentials:true 
   },
 });
